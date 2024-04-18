e4cea32924d146881d39ad750f752902f9ac5886
Tom di Mino <contact@tomdimino.com>
2024-04-17 21:20:18 -0400

    Modified  origin for Cors

--- web/app/api/tts.ts
+++ web/app/api/tts.ts
@@ -48,7 +48,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   };
 
   try {
-    const apiResponse = await fetch(`https://api.elevenlabs.io/v1/text-to-speech/${voice_id}`, options);
+    const apiResponse = await fetch(`https://api.elevenlabs.io/v1/text-to-speech/${voice_id}/stream`, options);
     if (!apiResponse.ok) throw new Error('Failed to convert text to speech');
 
     const audioBlob = await apiResponse.blob();
